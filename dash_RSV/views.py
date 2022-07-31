4ac28c219507ebcec705f5333c3717fbcdb7660a
nikitasencilo <senchilo.nd@yandex.ru>
2022-07-31 14:02:51 +0300

    31 july new app try

--- dash_RSV/views.py
+++ dash_RSV/views.py
@@ -3,10 +3,8 @@ from . import plotly_app
 from .models import prices_RSV_from_ATS
 # Create your views here.
 def index(request):
-    prices =prices_RSV_from_ATS.objects.all()
+    prices = prices_RSV_from_ATS.objects.all()
     return render(request,'dash_RSV/index.html', {'prices':prices})
 
-def about(request):
-    prices = prices_RSV_from_ATS.objects.all()
-    return render(request, 'dash_RSV/index.html', {'prices': prices})
+
 
