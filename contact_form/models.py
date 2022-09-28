172855f3e57ee52a617547bbfed4730bebcba175
nikitasencilo <senchilo.nd@yandex.ru>
2022-09-28 15:57:05 +0300

    new contact form

---
+++ contact_form/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    message = models.TextField(max_length=1000)
+
+    def __str__(self):
+        # Будет отображаться следующее поле в панели администрирования
+        return self.email
